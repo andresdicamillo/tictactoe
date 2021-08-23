65551a1024336594964f6c340d3d06e83d60b86d
Andres Di Camillo <andres.dicamillo@jointly.com>
2021-08-23 14:46:55 -0300

    Update game reducer

--- src/Tictactoe/gameReducer.js
+++ src/Tictactoe/gameReducer.js
@@ -1,40 +1,50 @@
 import checkWinner from './checkWinner'
 
+
+const startGame = () => {
+  return {
+    gameStarted: true,
+    xIsNext: true
+  }
+}
+
+const selectSquare = ({action, state}) => {
+  const {squares, xIsNext } = state
+  const {square} = action
+  const winner = checkWinner(squares)
+  if (winner || squares[square]) {
+    return state
+  }
+  const squaresCopy = [...squares]
+  squaresCopy[square] = xIsNext ? 'X' : 'O'
+  return {
+    squares: squaresCopy,
+    xIsNext: !xIsNext
+  }
+}
+
+const resetGame = () => {
+  return {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+    gameStarted: true
+  }
+}
+
+const methods = {
+  'START_GAME': startGame,
+  'SELECT_SQUARE': selectSquare,
+  'RESET_GAME': resetGame
+}
+
 const gameReducer = (state, action) => {
-    const {squares, xIsNext } = state
-    switch (action.type) {
-      case 'START_GAME': {
-        return {
-          gameStarted: true,
-          xIsNext: true
-        }
-      }
-      case 'SELECT_SQUARE': {
-        const {square} = action
-        const winner = checkWinner(squares)
-        if (winner || squares[square]) {
-          return state
-        }
-        const squaresCopy = [...squares]
-        squaresCopy[square] = xIsNext ? 'X' : 'O'
-        return {
-          squares: squaresCopy,
-          xIsNext: !xIsNext
-        }
-      }
-      case 'RESET_GAME': {
-        return {
-          squares: Array(9).fill(null),
-          xIsNext: true,
-          gameStarted: true
-        }
-      }
-      default: {
-        throw new Error(
-          `Unhandled action type: ${action.type}. Please fix it. Thank you.`,
-        )
-      }
-    }
+    try {
+      return methods[action.type]({action, state})
+    } catch(err) {
+      throw new Error(
+        `Unhandled action type: ${action.type}. Please fix it. Thank you.`,
+      )
+    }     
   }
 
-  export default gameReducer
+export default gameReducer
